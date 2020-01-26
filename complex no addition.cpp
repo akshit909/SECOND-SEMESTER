5e94dfa1cb3ca7f9b9ee81e244fc60758a8cd5d1
Akshit Gupta <55032419+akshit909@users.noreply.github.com>
2020-01-26 23:13:47 +0530

    Add files via upload

---
+++ complex no addition.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class complex
+{
+	int real 1,real 2;
+	int imag 1,imag 2;
+	public:
+		void read()
+		{
+		   cout<<"Enter the required numbers:\n";
+	        cin>>real 1;
+	        cin>>real 2;
+	          cin>>imag 1;
+	        cin>>imag 2;
+		}
+		void display()
+		{
+		   1st complex number=real 1+imag 1;
+		    2nd complex number=real 2+imag 2;
+		    cout<<1st complex number<<+<<2nd complex number;
+	}
+};
+   int main()
+   {
+   	complex obj;
+   	obj.dispay();
+   }
